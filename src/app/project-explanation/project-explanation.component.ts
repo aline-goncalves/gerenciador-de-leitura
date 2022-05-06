1eb8ed1f8a17d305c91da31d9bb29ad3aa1adead
aline.goncalves <aline.goncalves@db1.com.br>
2022-05-05 23:42:43 -0300

    criação e adição de dois componentes na tela inicial - atividade 05

---
+++ src/app/project-explanation/project-explanation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-explanation',
+  templateUrl: './project-explanation.component.html',
+  styleUrls: ['./project-explanation.component.css']
+})
+export class ProjectExplanationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
